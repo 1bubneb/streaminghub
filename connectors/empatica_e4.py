4365d88dc2782e8b3f5c8cd71513a54c9e3f9979
yasith <yasith@cs.odu.edu>
2019-10-19 13:51:08 -0400

    update code to fix error

--- connectors/empatica_e4.py
+++ connectors/empatica_e4.py
@@ -11,7 +11,7 @@ BUFFER_SIZE = 1024
 # tmp - Skin Temperature
 # bat - Device Battery
 # tag - Tag taken from the device
-STREAMS = {'acc', 'bvp', 'gsr', 'ibi', 'tmp', 'bat', 'tag'}
+STREAMS = ['acc', 'bvp', 'gsr', 'ibi', 'tmp', 'bat', 'tag']
 
 # Create socket connection
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,6 +26,8 @@ class STATES:
     DEVICES_FOUND__ = 'devices_found'
     CONNECTED_TO_DEVICE__ = "connected"
     READY_TO_SUBSCRIBE__ = "ready_to_subscribe"
+    SUBSCRIBE_COMPLETED__ = "subscribe completed"
+    STREAMING__ = 'streaming'
 
 
 # commands
@@ -39,7 +41,7 @@ class COMMANDS:
 # State
 STATE = STATES.NEW__
 DEVICE = None
-SUBSCRIBED_STREAMS = {}
+stream_i = 0
 
 
 def msg(s_: str) -> bytes:
@@ -70,67 +72,80 @@ def set_state(state: str):
 
 
 def process_incoming_msgs():
+    global stream_i
     in_msg: str = codecs.decode(s.recv(BUFFER_SIZE))
     # parse message(s)
     in_msg_cmds = [x.strip() for x in in_msg.split('\r\n')]
     for cmd in in_msg_cmds:
-        if cmd[0] != "R":
+        if len(cmd) == 0 or cmd.find(' ') == -1:
             continue
-        cmd = cmd[2:]
-        i = cmd.find(' ')
-        # DEVICE_LIST response
-        if cmd[:i] == COMMANDS.DEVICE_LIST__:
-            cmd = cmd[i + 1:]
-            # list devices connected
+        # Handle responses to request
+        if cmd[0] == 'R':
+            cmd = cmd[2:]
             i = cmd.find(' ')
-            num = int(cmd[:i]) if i != -1 else 0
-            devices = []
-            if num > 0:
-                cmds = cmd[i + 3:].split(' | ')
-                if len(cmds) != num:
-                    print('device count mismatch')
+            # DEVICE_LIST response
+            if cmd[:i] == COMMANDS.DEVICE_LIST__:
+                cmd = cmd[i + 1:]
+                # list devices connected
+                i = cmd.find(' ')
+                num = int(cmd[:i]) if i != -1 else 0
+                devices = []
+                if num > 0:
+                    cmds = cmd[i + 3:].split(' | ')
+                    if len(cmds) != num:
+                        print('device count mismatch')
+                        exit(1)
+                    devices = [x.split(' ') for x in cmds]
+                set_devices_connected(num, devices)
+            # DEVICE_CONNECT response
+            elif cmd[:i] == COMMANDS.DEVICE_CONNECT__:
+                cmd = cmd[i + 1:]
+                i = cmd.find(' ')
+                status = cmd[:i] if i != -1 else cmd
+                if status == "ERR":
+                    cmd = cmd[i + 1:]
+                    print('Error connecting to device: %s' % cmd)
                     exit(1)
-                devices = [x.split(' ') for x in cmds]
-            set_devices_connected(num, devices)
-        # DEVICE_CONNECT response
-        elif cmd[:i] == COMMANDS.DEVICE_CONNECT__:
-            cmd = cmd[i + 1:]
-            i = cmd.find(' ')
-            status = cmd[:i] if i != -1 else cmd
-            if status == "ERR":
+                elif status == "OK":
+                    print('Connected to device')
+                    set_state(STATES.CONNECTED_TO_DEVICE__)
+            # PAUSE response
+            elif cmd[:i] == COMMANDS.PAUSE__:
                 cmd = cmd[i + 1:]
-                print('Error connecting to device: %s' % cmd)
-                exit(1)
-            elif status == "OK":
-                print('Connected to device')
-                set_state(STATES.CONNECTED_TO_DEVICE__)
-        # PAUSE response
-        elif cmd[:i] == COMMANDS.PAUSE__:
-            cmd = cmd[i + 1:]
-            i = cmd.find(' ')
-            status = cmd[:i] if i != -1 else cmd
-            if status == "ERR":
+                i = cmd.find(' ')
+                status = cmd[:i] if i != -1 else cmd
+                if status == "ERR":
+                    cmd = cmd[i + 1:]
+                    print('Error pausing streaming: %s' % cmd)
+                    exit(1)
+                elif status == "ON":
+                    print('Streaming paused')
+                    set_state(STATES.READY_TO_SUBSCRIBE__)
+                elif status == "OFF":
+                    print('Streaming resumed')
+                    set_state(STATES.STREAMING__)
+            # DEVICE SUBSCRIBE response
+            elif cmd[:i] == COMMANDS.DEVICE_SUBSCRIBE__:
                 cmd = cmd[i + 1:]
-                print('Error pausing streaming: %s' % cmd)
-                exit(1)
-            elif status == "OK":
-                print('Streaming paused')
-                set_state(STATES.READY_TO_SUBSCRIBE__)
-        # DEVICE SUBSCRIBE response
-        elif cmd[:i] == COMMANDS.DEVICE_SUBSCRIBE__:
-            cmd = cmd[i + 1:]
-            i = cmd.find(' ')
-            stream_type = cmd[:i]
-            cmd = cmd[i + 1:]
-            i = cmd.find(' ')
-            status = cmd[:i] if i != -1 else cmd
-            if status == "ERR":
+                i = cmd.find(' ')
+                stream_type = cmd[:i]
                 cmd = cmd[i + 1:]
-                print('Error subscribing to stream %s: %s' % (stream_type, cmd))
-                exit(1)
-            elif status == "OK":
-                print('Subscribed to stream %s' % stream_type)
-                set_state(STATES.CONNECTED_TO_DEVICE__)
+                i = cmd.find(' ')
+                status = cmd[:i] if i != -1 else cmd
+                if status == "ERR":
+                    cmd = cmd[i + 1:]
+                    print('Error subscribing to stream %s: %s' % (stream_type, cmd))
+                    exit(1)
+                elif status == "OK":
+                    print('Subscribed: %s' % stream_type)
+                    stream_i += 1
+                    if stream_i == len(STREAMS):
+                        set_state(STATES.SUBSCRIBE_COMPLETED__)
+                    else:
+                        set_state(STATES.READY_TO_SUBSCRIBE__)
+        # Handle data stream
+        else:
+            print(cmd)
 
 
 while True:
@@ -139,7 +154,7 @@ while True:
         # request devices list
         print('Getting list of devices...')
         s.send(msg(COMMANDS.DEVICE_LIST__))
-        STATE = STATES.WAITING__
+        set_state(STATES.WAITING__)
     elif STATE == STATES.NO_DEVICES__:
         print('No devices found!')
         exit(1)
@@ -147,16 +162,23 @@ while True:
         # connect to device
         print('Connecting to device...')
         s.send(msg("%s %s" % (COMMANDS.DEVICE_CONNECT__, DEVICE)))
-        STATE = STATES.WAITING__
+        set_state(STATES.WAITING__)
     elif STATE == STATES.CONNECTED_TO_DEVICE__:
         # pause streaming initially
+        print('Initializing...')
         s.send(msg("%s ON" % COMMANDS.PAUSE__))
-        STATE = STATES.WAITING__
+        set_state(STATES.WAITING__)
     elif STATE == STATES.READY_TO_SUBSCRIBE__:
         # subscribe to streams
-        for stream in STREAMS:
-            s.send(msg("%s %s ON" % (COMMANDS.DEVICE_SUBSCRIBE__, stream)))
-        STATE = STATES.WAITING__
+        stream = STREAMS[stream_i]
+        print('Subscribing to stream: %s' % stream)
+        s.send(msg("%s %s ON" % (COMMANDS.DEVICE_SUBSCRIBE__, stream)))
+        set_state(STATES.WAITING__)
+    elif STATE == STATES.SUBSCRIBE_COMPLETED__:
+        # begin streaming data
+        print('Requesting data')
+        s.send(msg("%s OFF" % COMMANDS.PAUSE__))
+        set_state(STATES.STREAMING__)
     process_incoming_msgs()
 
 while True:
